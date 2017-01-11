648aff2e0115131df405dc1e6764cb4117040ed6
MurhafSousli <murhafsousli@gmail.com>
2017-01-11 11:32:00 +0300

    update demo

--- demo/src/app/header/header.component.ts
+++ demo/src/app/header/header.component.ts
@@ -6,13 +6,4 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
-  disqusLogo = prefixRepo('../../assets/img/disqus.svg');
-  angularLogo = prefixRepo('../../assets/img/angular-logo.png');
-
-
 }
-
-
-var prefixRepo = (path) => {
-  return 'ng2-disqus' + path;
-};
