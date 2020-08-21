51fc63dec880a3fca4c2a51dbd543e62acd0a75f
Stepan Suvorov <stevermeister@gmail.com>
2020-08-21 13:17:54 +0200

    cleaup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { HighlightModule } from 'ngx-highlightjs';
 import { DisqusModule } from 'ngx-disqus';
 import { AppComponent } from './app.component';
 import { DocsComponent } from './docs/docs.component';
-
-import xml from 'highlight.js/lib/languages/xml';
-import typescript from 'highlight.js/lib/languages/typescript';
-
-export function languages() {
-  return [
-    {name: 'xml', func: xml},
-    {name: 'typescript', func: typescript },
-  ]
-}
 
 @NgModule({
   declarations: [
@@ -24,8 +13,7 @@ export function languages() {
   imports: [
     BrowserModule,
     CommonModule,
-    DisqusModule.forRoot('ngx'),
-    HighlightModule.forRoot({ languages })
+    DisqusModule.forRoot('ngx')
   ],
   bootstrap: [AppComponent]
 })
