fd601840cec305a6ef96350d705cc106c74b130b
Murhaf Sousli <murhafsousli@gmail.com>
2023-07-10 03:18:46 +0300

    Upgrade to Angular 16

--- projects/ngx-disqus/src/public_api.ts
+++
@@ -1,3 +0,0 @@
-export { DisqusModule } from './lib/disqus.module';
-export { DisqusComment, DisqusReady, DISQUS_SHORTNAME } from './lib/disqus.model';
-export { DisqusService } from './lib/disqus.service';
